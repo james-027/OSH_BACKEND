8998bb0f5310d112802a569838cc4c3c2cc1af1f
AljAtok <143077470+AljAtok@users.noreply.github.com>
2026-05-06 20:37:27 +0800

    Reverting back to client side rendering endpoint

--- src/modules/users/controllers/user-audit-trail.controller.ts
+++ src/modules/users/controllers/user-audit-trail.controller.ts
@@ -16,22 +16,28 @@ import { UserAuditTrailService } from "../services/user-audit-trail.service";
 export class UserAuditTrailController {
   constructor(private readonly userAuditTrailService: UserAuditTrailService) {}
 
+  // @Get()
+  // @RequirePermissions({ module: "AUDIT TRAIL", action: "VIEW" })
+  // async findAll(
+  //   @Query('page') page: number = 0,
+  //   @Query('pageSize') pageSize: number = 10,
+  //   @Query('sortBy') sortBy: string = 'created_at',
+  //   @Query('sortOrder') sortOrder: 'asc' | 'desc' = 'desc',
+  //   @Query('searchQuery') searchQuery?: string,
+  // ) {
+  //   return this.userAuditTrailService.findAll(
+  //     page,
+  //     pageSize,
+  //     sortBy,
+  //     sortOrder,
+  //     searchQuery,
+  //   );
+  // }
+
   @Get()
   @RequirePermissions({ module: "AUDIT TRAIL", action: "VIEW" })
-  async findAll(
-    @Query('page') page: number = 0,
-    @Query('pageSize') pageSize: number = 10,
-    @Query('sortBy') sortBy: string = 'created_at',
-    @Query('sortOrder') sortOrder: 'asc' | 'desc' = 'desc',
-    @Query('searchQuery') searchQuery?: string,
-  ) {
-    return this.userAuditTrailService.findAll(
-      page,
-      pageSize,
-      sortBy,
-      sortOrder,
-      searchQuery,
-    );
+  async findAll() {
+    return this.userAuditTrailService.findAllOld();
   }
 
   @Get(":id")
