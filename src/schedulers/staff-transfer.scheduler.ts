5fa8d0ef3c9e1145a1e11e10293f10dd7e134350
AljAtok-C <akatok@bounty.com.ph@github.com>
2026-07-14 22:25:11 +0800

    Conditional CRON usage on expensive CRON usage

--- src/schedulers/staff-transfer.scheduler.ts
+++ src/schedulers/staff-transfer.scheduler.ts
@@ -2,14 +2,14 @@ import { Injectable } from "@nestjs/common";
 import { Cron, CronExpression } from "@nestjs/schedule";
 import logger from "../config/logger";
 import { StaffsService } from "src/modules/staffs/services/staffs.service";
+import { ConditionalCron } from "src/decorators/conditional-cron.decorator";
 
 @Injectable()
 export class StaffTransferScheduler {
-  constructor(
-    private readonly staffsService: StaffsService,
-  ) {}
+  constructor(private readonly staffsService: StaffsService) {}
 
-  @Cron(CronExpression.EVERY_SECOND)
+  // @Cron(CronExpression.EVERY_SECOND)
+  @ConditionalCron(CronExpression.EVERY_SECOND, "ENABLE_STAFF_TRANSFER_CRON")
   async processScheduledTransfers() {
     try {
       await this.staffsService.processScheduledTransfers();
